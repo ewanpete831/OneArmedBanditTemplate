833f02f3c9bc61e58d32ae2e8e70e93f9c572f4b
ewanpete831 <ewanpete831@outlook.com>
2021-05-06 14:54:43 -0400

    finished code

--- OneArmedBandit/Form1.Designer.cs
+++ OneArmedBandit/Form1.Designer.cs
@@ -46,10 +46,9 @@
             this.spinButton.BackColor = System.Drawing.Color.LimeGreen;
             this.spinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.spinButton.Font = new System.Drawing.Font("Rockwell", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.spinButton.Location = new System.Drawing.Point(319, 122);
-            this.spinButton.Margin = new System.Windows.Forms.Padding(2);
+            this.spinButton.Location = new System.Drawing.Point(478, 188);
             this.spinButton.Name = "spinButton";
-            this.spinButton.Size = new System.Drawing.Size(51, 46);
+            this.spinButton.Size = new System.Drawing.Size(76, 71);
             this.spinButton.TabIndex = 3;
             this.spinButton.Text = "SPIN";
             this.spinButton.UseVisualStyleBackColor = false;
@@ -60,10 +59,9 @@
             this.outputLabel.BackColor = System.Drawing.Color.DimGray;
             this.outputLabel.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.outputLabel.ForeColor = System.Drawing.Color.White;
-            this.outputLabel.Location = new System.Drawing.Point(104, 236);
-            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Location = new System.Drawing.Point(156, 363);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(162, 31);
+            this.outputLabel.Size = new System.Drawing.Size(243, 48);
             this.outputLabel.TabIndex = 4;
             this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -72,10 +70,9 @@
             this.scoreDisplay.BackColor = System.Drawing.Color.Transparent;
             this.scoreDisplay.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.scoreDisplay.ForeColor = System.Drawing.Color.White;
-            this.scoreDisplay.Location = new System.Drawing.Point(59, 236);
-            this.scoreDisplay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.scoreDisplay.Location = new System.Drawing.Point(88, 363);
             this.scoreDisplay.Name = "scoreDisplay";
-            this.scoreDisplay.Size = new System.Drawing.Size(41, 31);
+            this.scoreDisplay.Size = new System.Drawing.Size(62, 48);
             this.scoreDisplay.TabIndex = 5;
             this.scoreDisplay.Text = "10";
             this.scoreDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -85,10 +82,9 @@
             this.coinLabel.BackColor = System.Drawing.Color.Transparent;
             this.coinLabel.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.coinLabel.ForeColor = System.Drawing.Color.White;
-            this.coinLabel.Location = new System.Drawing.Point(11, 235);
-            this.coinLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.coinLabel.Location = new System.Drawing.Point(16, 362);
             this.coinLabel.Name = "coinLabel";
-            this.coinLabel.Size = new System.Drawing.Size(56, 31);
+            this.coinLabel.Size = new System.Drawing.Size(84, 48);
             this.coinLabel.TabIndex = 6;
             this.coinLabel.Text = "Coins:";
             this.coinLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -98,9 +94,10 @@
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.BackgroundImage = global::OneArmedBandit.Properties.Resources.diamond_100x125;
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(235, 116);
+            this.pictureBox3.Location = new System.Drawing.Point(352, 178);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox3.Size = new System.Drawing.Size(81, 86);
             this.pictureBox3.TabIndex = 12;
             this.pictureBox3.TabStop = false;
             // 
@@ -109,9 +106,10 @@
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.BackgroundImage = global::OneArmedBandit.Properties.Resources.cherry_100x125;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(159, 116);
+            this.pictureBox2.Location = new System.Drawing.Point(238, 178);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox2.Size = new System.Drawing.Size(81, 86);
             this.pictureBox2.TabIndex = 11;
             this.pictureBox2.TabStop = false;
             // 
@@ -120,19 +118,20 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::OneArmedBandit.Properties.Resources._7_100x125;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(84, 116);
+            this.pictureBox1.Location = new System.Drawing.Point(126, 178);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox1.Size = new System.Drawing.Size(81, 86);
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::OneArmedBandit.Properties.Resources.slotBack;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(373, 268);
+            this.ClientSize = new System.Drawing.Size(560, 412);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -141,7 +140,6 @@
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.spinButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "One Armed Bandit";
--- OneArmedBandit/Form1.cs
+++ OneArmedBandit/Form1.cs
@@ -12,9 +12,9 @@ namespace OneArmedBandit
     public partial class Form1 : Form
     {
         // random number generator
-
+        Random randomNum = new Random();
         // int value for score initialized to 10
-
+        int score = 10;
 
         public Form1()
         {
@@ -24,19 +24,54 @@ namespace OneArmedBandit
         private void spinButton_Click(object sender, EventArgs e) 
         {
             // get random values for each reel (store each in separate int variable)
-
+            int reel1 = randomNum.Next(1, 4);
+            int reel2 = randomNum.Next(1, 4);
+            int reel3 = randomNum.Next(1, 4);
 
             // check value of reel 1 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
-
+            switch(reel1)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._7_100x125;
+                    break;
+            }
+            
 
             // check value of reel 2 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
-
+            switch (reel2)
+            {
+                case 1:
+                    pictureBox2.Image = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox2.Image = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox2.Image = Properties.Resources._7_100x125;
+                    break;
+            }
             // check value of reel 3 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
-
-
+            switch (reel3)
+            {
+                case 1:
+                    pictureBox3.Image = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox3.Image = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox3.Image = Properties.Resources._7_100x125;
+                    break;
+            }
             /// STOP HERE ----------------------------------------------------------
             /// Test to make sure that your program will display random
             /// images to each reel. Only continue on after you know this works
@@ -45,14 +80,40 @@ namespace OneArmedBandit
 
             // Use compound if statement to check if all reels are equal. 
             // If yes show "winner" statement and add 3 to score.
-            // If no show "play again" statement and subtract 1 from score.         
+            // If no show "play again" statement and subtract 1 from score.
+
+            if (reel1 == 1 && reel2 == 1 && reel3 == 1)
+            {
+                outputLabel.Text = "Winner";
+                score = score + 3;
+            }
+            else if (reel1 == 2 && reel2 == 2 && reel3 == 2)
+            {
+                outputLabel.Text = "Winner";
+                score = score + 3;
+            }
+            else if (reel1 == 3 && reel2 == 3 && reel3 == 3)
+            {
+                outputLabel.Text = "Winner";
+                score = score + 3;
+            }
+            else
+            {
+                outputLabel.Text = "Try Again";
+                score--;
+            }
 
 
             // if score has reached 0 display "lose" message and set button enabled property to false
-
+           if(score == 0)
+            {
+                outputLabel.Text = "Out of coins!";
+                spinButton.Enabled = false;
+                spinButton.BackColor = Color.Red;
+            }
 
             // display updated score
-
+            scoreDisplay.Text = $"{score}";
         }
     }
 }
